34cdb687adb91c5732b7e3791cf5f7ca08ed54d0
Victor Jacks <vjacks@e3r6p4.21-school.ru>
2018-12-28 20:29:10 +0300

    multiple files. one function

--- get_next_line.c
+++ get_next_line.c
@@ -11,57 +11,105 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-//#include <stdio.h>
-//#include <fcntl.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+
+// int	get_next_line(const int fd, char **line)
+// {
+// 	static char *buf;
+// 	char byte[1];
+// 	char *chunk = NULL;
+// 	char *sub = NULL;
+// 	char *prev = NULL;
+
+// 	CHECK_ERROR((line && BUFF_SIZE > 0 && fd >= 0 && fd < FD_LIMIT && read(fd, byte, 0) >= 0));
+// 	if (!buf)
+// 		CHECK_ERROR((buf = ft_strnew(BUFF_SIZE)));
+// 	if (buf[0] == '\0')
+// 		buf[read(fd, buf, BUFF_SIZE)] = '\0';
+// 	if (buf[0] == '\0')
+// 	{
+// 		ft_memdel((void **)&buf);
+// 		return (0);
+// 	}
+// 	CHECK_ERROR((*line = ft_strnew(0)));
+	
+// //	printf("BUF: |%s|\n", buf);
+// 	while (buf[0] != '\0')
+// 	{
+// 		sub = ft_strchr(buf, '\n');
+// 		if (sub)
+// 		{
+// 			CHECK_ERROR((chunk = ft_strnew(sub - buf)));
+// 			ft_strncpy(chunk, buf, sub - buf);
+// 			prev = *line;
+// 			*line = ft_strjoin(prev, chunk);
+// 			ft_memdel((void **)&chunk);
+// 			CHECK_ERROR(*line);
+// 			ft_memdel((void **)&prev);
+// 			ft_strcpy(buf, sub + 1);
+// //			printf("BUFF AFTER: |%s|\n", buf);
+// 			return (1);
+// 		}
+// 		prev = *line;
+// 		*line = ft_strjoin(prev, buf);
+// 		CHECK_ERROR(*line);
+// 		ft_memdel((void **)&prev);
+// 		buf[read(fd, buf, BUFF_SIZE)] = '\0';
+// //		printf("NEW BUF: |%s|\n", buf);
+// 	}
+// 	return (1);
+// }
 
 int	get_next_line(const int fd, char **line)
 {
-	static char *buf;
+	static char **buffers;
 	char byte[1];
 	char *chunk = NULL;
 	char *sub = NULL;
 	char *prev = NULL;
 
 	CHECK_ERROR((line && BUFF_SIZE > 0 && fd >= 0 && fd < FD_LIMIT && read(fd, byte, 0) >= 0));
-	if (!buf)
-		CHECK_ERROR((buf = ft_strnew(BUFF_SIZE)));
-	if (buf[0] == '\0')
-		buf[read(fd, buf, BUFF_SIZE)] = '\0';
-
-	if (buf[0] == '\0')
+	if (!buffers)
+		CHECK_ERROR(buffers = (char **)malloc(sizeof(char *) * FD_LIMIT));
+	if (!buffers[fd])
+		buffers[fd] = ft_strnew(1);
+	if (*(buffers[fd]) == '\0')
+		buffers[fd][read(fd, buffers[fd], BUFF_SIZE)] = '\0';
+	if (*(buffers[fd]) == '\0')
 	{
-		ft_memdel((void **)&buf);
+		ft_memdel((void **)&*buffers);
 		return (0);
 	}
 	CHECK_ERROR((*line = ft_strnew(0)));
 	
-//	printf("BUF: |%s|\n", buf);
-	while (buf[0] != '\0')
+//	printf("*buffers: |%s|\n", *buffers);
+	while (*(buffers[fd]) != '\0')
 	{
-		sub = ft_strchr(buf, '\n');
+		sub = ft_strchr(buffers[fd], '\n');
 		if (sub)
 		{
-			CHECK_ERROR((chunk = ft_strnew(sub - buf)));
-			ft_strncpy(chunk, buf, sub - buf);
+			CHECK_ERROR((chunk = ft_strnew(sub - buffers[fd])));
+			ft_strncpy(chunk, buffers[fd], sub - buffers[fd]);
 			prev = *line;
 			*line = ft_strjoin(prev, chunk);
 			ft_memdel((void **)&chunk);
 			CHECK_ERROR(*line);
 			ft_memdel((void **)&prev);
-			ft_strcpy(buf, sub + 1);
-//			printf("BUFF AFTER: |%s|\n", buf);
+			ft_strcpy(buffers[fd], sub + 1);
+//			printf("*buffersF AFTER: |%s|\n", *buffers);
 			return (1);
 		}
 		prev = *line;
-		*line = ft_strjoin(prev, buf);
+		*line = ft_strjoin(prev, buffers[fd]);
 		CHECK_ERROR(*line);
 		ft_memdel((void **)&prev);
-		buf[read(fd, buf, BUFF_SIZE)] = '\0';
-//		printf("NEW BUF: |%s|\n", buf);
+		buffers[fd][read(fd, buffers[fd], BUFF_SIZE)] = '\0';
+//		printf("NEW *buffers: |%s|\n", *buffers);
 	}
 	return (1);
 }
-
 
 // int	main(int argc, char **argv)
 // {
