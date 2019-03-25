793133fcf86a7254d68f7e1f6ec1aeb0f9b704b4
Nightwatch95 <somenightwatch@gmail.com>
2019-03-25 15:29:29 +0300

    Norminated

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vjacks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 16:12:46 by vjacks            #+#    #+#             */
-/*   Updated: 2018/12/21 16:13:32 by vjacks           ###   ########.fr       */
+/*   Updated: 2019/03/25 14:19:23 by vjacks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void	init_buffers(char **buffers)
 {
-	int	i;
+	int		i;
 
 	i = -1;
 	while (++i < FD_LIMIT)
@@ -23,9 +23,9 @@ static void	init_buffers(char **buffers)
 
 static int	read_line(char **buf, int fd, char **line)
 {
-	char *chunk;
-	char *sub;
-	char *prev;
+	char	*chunk;
+	char	*sub;
+	char	*prev;
 
 	while ((*buf)[0] != '\0')
 	{
@@ -35,7 +35,7 @@ static int	read_line(char **buf, int fd, char **line)
 			CHECK_ERROR((chunk = ft_strnew(sub - *buf)));
 			ft_strncpy(chunk, *buf, sub - *buf);
 			prev = *line;
-			CHECK_ERROR((*line = ft_strjoin(prev, chunk)));			
+			CHECK_ERROR((*line = ft_strjoin(prev, chunk)));
 			ft_memdel((void **)&chunk);
 			ft_memdel((void **)&prev);
 			ft_strcpy(*buf, sub + 1);
@@ -49,9 +49,9 @@ static int	read_line(char **buf, int fd, char **line)
 	return (1);
 }
 
-int	get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
-	static char **buffers;
+	static char	**buffers;
 	char		byte[1];
 
 	CHECK_ERROR(line && BUFF_SIZE > 0 && fd >= 0 &&
@@ -60,7 +60,7 @@ int	get_next_line(const int fd, char **line)
 	{
 		CHECK_ERROR((buffers = (char**)malloc(sizeof(char*) * FD_LIMIT)));
 		init_buffers(buffers);
-	}	
+	}
 	if (!buffers[fd])
 		buffers[fd] = ft_strnew(BUFF_SIZE);
 	if (buffers[fd][0] == '\0')
@@ -74,24 +74,3 @@ int	get_next_line(const int fd, char **line)
 	CHECK_ERROR(read_line(&buffers[fd], fd, line));
 	return (1);
 }
-
-// #include <fcntl.h>
-
-// int	main(int argc, char **argv)
-// {
-// 	char *line = NULL;
-// 	CHECK_ERROR(argc > 1);
-// 	int	fd1 = open(argv[1], O_RDONLY);
-// 	int fd2 = open(argv[2], O_RDONLY);
-// 	int	ret = 0;
-
-// 	for (int i = 1; i <= 9; i++)
-// 	{
-// 		int fd = i % 2 == 0 ? fd1 : fd2;
-
-// 		ret = get_next_line(fd, &line);
-// 		printf("ret: %d, fd: %d, line: %s\n",  ret, fd, line);
-// 		ft_memdel((void **)&line);
-// 	}
-// 	return (0);
-// }
